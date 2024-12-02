d6c5b12a43506ca9d7a93b924da6d34f779c72ac
Bryan2627123 <bryan.pumacayo@tecsup.edu.pe>
2024-12-02 09:48:54 -0500

    Inicialización de la referencia a la base de datos

--- app/src/main/java/com/example/proyectotemperatura/MainActivity.kt
+++ app/src/main/java/com/example/proyectotemperatura/MainActivity.kt
@@ -27,10 +27,10 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Solicitar permiso para notificaciones
+        // Aquí se solicita permiso para notificaciones
         requestNotificationPermission()
 
-        // Inicializa la referencia a la base de datos
+        // En esta parte se inicializa la referencia a la base de datos
         database = FirebaseDatabase.getInstance().reference
 
         // Aquí se crea el canal de notificaciones
